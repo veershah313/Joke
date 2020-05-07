ce8f79a1a0bef09d88fe83fa2192d428c3562ffa
Veer Shah <veer.shah.uk@gmail.com>
2020-05-07 21:59:56 +0100

    Add project files.

---
+++ Joke/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Joke
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Knock! Knock!");
+            Console.ReadLine();
+            Console.WriteLine("Boo");
+            Console.ReadLine();
+            Console.WriteLine("No need to cry, it's just a knock, knock joke!");
+
+        }
+    }
+}
